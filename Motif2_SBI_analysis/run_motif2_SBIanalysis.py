7178aa68cea2a7ec32a99caea5fa1f543b130e49
Ananya Muguli <40715049+Ananyamuguli@users.noreply.github.com>
2025-11-20 13:03:29 -0600

    Motif 2 SBI analysis codes

---
+++ Motif2_SBI_analysis/run_motif2_SBIanalysis.py
@@ -0,0 +1,28 @@
+import sys
+from motif2_SBIanalysis import *
+
+
+if len(sys.argv)==3:
+    if int(sys.argv[1]) == 1:
+        print('Glutamate analysis')
+        glutamate_analysis(sys.argv[2])
+        print('-------------Done---------------')
+    if int(sys.argv[1]) == 2:
+        print('ATP')
+        atp_dependency(sys.argv[2])
+        print('-------------Done---------------')
+    if int(sys.argv[1]) == 3:
+        print('Mito')
+        mito_dependency(sys.argv[2])
+        print('-------------Done---------------')
+    if int(sys.argv[1]) == 4:
+        print('Ko')
+        Ko_dependency(sys.argv[2])
+        print('-------------Done---------------')
+    if int(sys.argv[1]) == 5:
+        print('Synaptic weight')
+        synweight_dependency(sys.argv[2])
+        print('-------------Done---------------')                
+else:
+    print('Wrong set of inputs in the input command')
+
